a57255a640c9bfd5e4786c42b430acc842344a37
Odebowale Opeyemi <104089729+Opski123@users.noreply.github.com>
2022-06-22 11:52:35 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+let myName = 'Name: Odebowale Opeyemi\n';
+console.log(myName);
+
+let height = 'Height: 158cm\n';
+console.log(height);
+
+let country = 'Country: Nigeria ';
+console.log(country);
+
+alert (myName + height + country);
